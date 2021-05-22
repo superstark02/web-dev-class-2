523b83d3fce89223daee857f444bdae8e06ed0d3
superstark02 <superstark02@gmail.com>
2021-05-22 15:12:36 +0530

    123

---
+++ src/Components/MyAppBar.js
@@ -0,0 +1,24 @@
+import './MyAppBar.css';
+
+export default function MyAppBar() {
+  return (
+    <div className="app-bar">
+      <h1>
+          App Bar
+      </h1>
+    </div>
+  );
+}
+
+export function Footer(){
+    return (
+        <div className="app-bar">
+          <h1>
+              Footer
+          </h1>
+        </div>
+      );
+}
+
+
+
